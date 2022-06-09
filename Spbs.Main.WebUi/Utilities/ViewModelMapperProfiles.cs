b3eceec85a8c82774faa98201b5b97f8d9330dd6
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-06-09 15:15:06 +0200

    Added automapper profile for purchase view model

---
+++ Spbs.Main.WebUi/Utilities/ViewModelMapperProfiles.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Spbs.Main.Core.Models;
+using Spbs.Main.WebUi.ViewModels;
+
+namespace Spbs.Main.WebUi.Utilities;
+
+public class ViewModelMapperProfiles : Profile
+{
+    public ViewModelMapperProfiles()
+    {
+        CreateMap<NewPurchaseViewModel, Purchase>()
+            .ForMember(p => p.Id, a => a.Ignore())
+            .ForMember(p => p.OwnerId, a => a.Ignore())
+            .ForMember(p => p.Items, a => a.Ignore());
+    }
+}
