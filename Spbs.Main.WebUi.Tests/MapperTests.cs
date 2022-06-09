6289d5f3af5a26d718469f8b0ef766ee390315ed
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-06-09 15:15:52 +0200

    Added a test project for the Ui project, added a basic unit test for the view model mapper

---
+++ Spbs.Main.WebUi.Tests/MapperTests.cs
@@ -0,0 +1,22 @@
+using AutoMapper;
+using Spbs.Main.WebUi.Utilities;
+using Xunit; 
+
+namespace Spbs.Main.WebUi.Tests;
+
+public class MapperTests
+{
+    private MapperConfiguration GetMapperConfiguration()
+    {
+        return new MapperConfiguration(mapperconfig =>
+        {
+            mapperconfig.AddProfile(new ViewModelMapperProfiles());
+        });
+    }
+
+    public void MapperConfiguration_ShouldBeValid()
+    {
+        var configuration = GetMapperConfiguration();
+        configuration.AssertConfigurationIsValid();
+    }
+}
