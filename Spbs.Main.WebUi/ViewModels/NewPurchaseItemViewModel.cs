b2ccea5eac68c595f155e2fc0676f9b87628cbe2
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-06-13 16:12:06 +0200

    Added view model for purchase items

---
+++ Spbs.Main.WebUi/ViewModels/NewPurchaseItemViewModel.cs
@@ -0,0 +1,9 @@
+namespace Spbs.Main.WebUi.ViewModels;
+
+public class NewPurchaseItemViewModel
+{
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string ProductName { get; set; }
+    //public List<string> Categories { get; set; }
+}
