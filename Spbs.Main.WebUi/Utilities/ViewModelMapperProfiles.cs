9d1b6a2356e3872691f7917cb3bb9509d0000701
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2022-06-14 16:06:58 +0200

    Ignore the total field

--- Spbs.Main.WebUi/Utilities/ViewModelMapperProfiles.cs
+++ Spbs.Main.WebUi/Utilities/ViewModelMapperProfiles.cs
@@ -11,7 +11,8 @@ public class ViewModelMapperProfiles : Profile
         CreateMap<NewPurchaseViewModel, Purchase>()
             .ForMember(p => p.Id, a => a.Ignore())
             .ForMember(p => p.OwnerId, a => a.Ignore())
-            .ForMember(p => p.Items, a => a.Ignore());
+            .ForMember(p => p.Items, a => a.Ignore())
+            .ForMember(p => p.Total, action => action.Ignore());
         CreateMap<NewPurchaseItemViewModel, PurchaseItem>();
     }
 }
