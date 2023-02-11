19c99bb1602e4e6b40427e1b8392e7415214dc5f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-02-11 12:05:35 +0100

    Implement filter functionality of recurring expenses

--- Spbs.Ui/Features/RecurringExpenses/RecurringExpensesOverview.razor.cs
+++ Spbs.Ui/Features/RecurringExpenses/RecurringExpensesOverview.razor.cs
@@ -2,5 +2,13 @@ namespace Spbs.Ui.Features.RecurringExpenses;
 
 public partial class RecurringExpensesOverview
 {
-    
+    private RecurringExpensesListComponent _recurringExpensesList;
+
+    private void SetFocusRecurrenceType(RecurrenceType? type)
+    {
+        _recurringExpensesList.SetFilter(new()
+        {
+            RecurrenceType = type
+        });
+    }
 }
