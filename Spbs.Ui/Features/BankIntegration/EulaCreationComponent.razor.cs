6b7c2ff9c597ea5d8418148cdfa439c5a700b4fa
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-03-10 23:44:58 +0100

    Start making a eula creation componen

--- Spbs.Ui/Features/BankIntegration/EulaCreationComponent.razor.cs
+++ Spbs.Ui/Features/BankIntegration/EulaCreationComponent.razor.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Integrations.Nordigen;
+using Microsoft.AspNetCore.Components;
 using Spbs.Ui.Features.BankIntegration.Models;
 
 namespace Spbs.Ui.Features.BankIntegration;
 
+public class EulaCreationViewModel
+{
+    
+}
+
 public partial class EulaCreationComponent : ComponentBase
 {
     private NordigenEula _eula = new();
+
+    [Inject] private INordigenApiClient NordigenClient { get; set; }
 
     private void HandleValidSubmit()
     {
