ff115a300c5d5df324122695e67d492ebaa64d7f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-03-11 11:15:08 +0100

    Add authentication state extension attribute

--- Spbs.Ui/Features/RecurringExpenses/UpcomingRecurringExpensesComponent.razor.cs
+++ Spbs.Ui/Features/RecurringExpenses/UpcomingRecurringExpensesComponent.razor.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
+using Spbs.Generators.UserExtensions;
 using Spbs.Ui.Auth;
 
 namespace Spbs.Ui.Features.RecurringExpenses;
 
+[AuthenticationTaskExtension()]
 public partial class UpcomingRecurringExpensesComponent
 {
     [Inject] public IRecurringExpenseReaderRepository RecurringExpenseReaderRepository { get; set; }
-    [CascadingParameter] private Task<AuthenticationState> authenticationStateTask { get; set; }
 
     private List<RecurringExpense> _upcomingExpenses = new();
     
@@ -21,20 +22,5 @@ public partial class UpcomingRecurringExpensesComponent
 
         int today = DateTime.Now.Day;
         _upcomingExpenses = await RecurringExpenseReaderRepository.GetRecurringExpensesAfterDayOfMonth(userId.Value, today);
-    }
-
-    private Guid? _cachedUserId = null;
-    private async Task<Guid?> UserId()
-    {
-        if (_cachedUserId is not null)
-        {
-            return _cachedUserId;
-        }
-        
-        var authState = await authenticationStateTask;
-        var user = authState.User;
-
-        _cachedUserId = user.GetUserId();
-        return _cachedUserId;
     }
 }
