cb09e2afca77be5498bb403e1340ce8bb27dc1e7
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-04-01 12:30:47 +0200

    Add logic to fetch user links on load

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
@@ -1,13 +1,24 @@
+#nullable enable
+
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Spbs.Generators.UserExtensions;
+using Spbs.Ui.Features.BankIntegration.Models;
 
 namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 
+[AuthenticationTaskExtension]
 public partial class LinksOverviewPage
 {
-    
-    
+    [Inject] private INordigenLinkReaderRepository _linkReader { get; set; }
+
+    private ReadOnlyCollection<NordigenLink>? _userLinks;
+
     protected override async Task OnInitializedAsync()
     {
-        
+        Guid? userId = await UserId();
+        _userLinks = await _linkReader.GetLinksForUser(userId!.Value);
     }
 }
