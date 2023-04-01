685b84168a4b48e085754b6447e2676c0bc697e4
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-04-01 11:51:00 +0200

    Add sub menu for bank integration items

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinkDetailsComponent.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinkDetailsComponent.razor.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Components;
 
 namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 
-public partial class LinkViewComponent
+public partial class LinkDetailsComponent
 {
     [Parameter] public string RequisitionId { get; set; }
 }
