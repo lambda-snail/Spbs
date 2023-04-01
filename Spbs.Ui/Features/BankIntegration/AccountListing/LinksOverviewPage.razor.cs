dd1bbffa3d790cde4f715ff700237ed96f29d9ad
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-04-01 13:46:50 +0200

    Add sandbox institution for debug sessions

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
@@ -1,8 +1,11 @@
 #nullable enable
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using AutoMapper;
+using Integrations.Nordigen;
 using Microsoft.AspNetCore.Components;
 using Spbs.Generators.UserExtensions;
 using Spbs.Ui.Features.BankIntegration.Models;
@@ -13,12 +16,35 @@ namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 public partial class LinksOverviewPage
 {
     [Inject] private INordigenLinkReaderRepository _linkReader { get; set; }
-
+    [Inject] private INordigenApiClient _nordigenCLient { get; set; }
+    [Inject] private IMapper _mapper { get; set; }
+    
     private ReadOnlyCollection<NordigenLink>? _userLinks;
-
+    private ReadOnlyCollection<Institution>? _institutions;
+    
     protected override async Task OnInitializedAsync()
+    {
+        await InitInstitutionList();
+        await GetLinksForUser();
+        StateHasChanged();
+    }
+
+    private async Task GetLinksForUser()
     {
         Guid? userId = await UserId();
         _userLinks = await _linkReader.GetLinksForUser(userId!.Value);
+    }
+
+    private async Task InitInstitutionList()
+    {
+        // TODO: Remove hard coded country
+        var aspsps = await _nordigenCLient.GetListOfInstitutionsAsync("SE");
+        var institutions = _mapper.Map<List<Institution>>(aspsps);
+        
+#if DEBUG
+        institutions.Add(new Institution { Name = "Sandbox", Id = "SANDBOXFINANCE_SFIN0000", Bic = string.Empty });
+#endif
+        
+        _institutions = new(institutions);
     }
 }
