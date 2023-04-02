9b18dd8998c8f783f735e5cd4b380bfdd507d8c6
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-04-02 13:46:27 +0200

    Add selectable card to display linked institutions

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
@@ -3,21 +3,25 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Integrations.Nordigen;
 using Microsoft.AspNetCore.Components;
 using Spbs.Generators.UserExtensions;
+using Spbs.Ui.Components;
 using Spbs.Ui.Features.BankIntegration.Models;
 
 namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 
 [AuthenticationTaskExtension]
-public partial class LinksOverviewPage
+public partial class LinksOverviewPage : SelectableListComponent<NordigenLink>
 {
     [Inject] private INordigenLinkReaderRepository _linkReader { get; set; }
     [Inject] private INordigenApiClient _nordigenCLient { get; set; }
     [Inject] private IMapper _mapper { get; set; }
+    
+    protected override List<NordigenLink>? GetList() => _userLinks.ToList();
     
     private ReadOnlyCollection<NordigenLink>? _userLinks;
     private ReadOnlyCollection<Institution>? _institutions;
@@ -27,6 +31,11 @@ public partial class LinksOverviewPage
         await InitInstitutionList();
         await GetLinksForUser();
         StateHasChanged();
+    }
+    
+    private string GetRowClass(int i)
+    {
+        return GetSelected() == i ? "bg-secondary text-white" : string.Empty;
     }
 
     private async Task GetLinksForUser()
