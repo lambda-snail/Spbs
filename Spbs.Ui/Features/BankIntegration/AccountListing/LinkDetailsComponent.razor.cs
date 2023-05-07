7e661716ec99b74c28c6b4abda5a03fb32a3e342
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-05-07 13:48:34 +0200

    Add link details

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinkDetailsComponent.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinkDetailsComponent.razor.cs
@@ -1,8 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Integrations.Nordigen;
+using Integrations.Nordigen.Models;
 using Microsoft.AspNetCore.Components;
+using Spbs.Generators.UserExtensions;
+using Spbs.Ui.Components;
+using Spbs.Ui.Features.BankIntegration.Models;
+using Spbs.Ui.Features.BankIntegration.Services;
 
 namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 
-public partial class LinkDetailsComponent
+[AuthenticationTaskExtension]
+public partial class LinkDetailsComponent : SelectableListComponent<Guid>
 {
     [Parameter] public string RequisitionId { get; set; }
+
+    [Inject] private INordigenApiClient _client { get; set; }
+    [Inject] private INordigenLinkReaderRepository _linkReader { get; set; }
+
+    private NordigenLink? _link;
+    private List<AccountV2> _accounts = new();
+    
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+
+        Guid? userId = await UserId();
+        ArgumentNullException.ThrowIfNull(userId, "UserId");
+        
+        Guid linkId = Guid.Parse(RequisitionId);
+        _link = await _linkReader.GetLinkById(linkId, userId.Value);
+
+        if (_link is { Accounts: not null and { Count: > 0 } })
+        {
+            _accounts = new(_link.Accounts.Count);
+            foreach (var accountId in _link.Accounts)
+            {
+                var account = await _client.GetAccountMetadata(accountId);
+                _accounts.Add(account);
+                StateHasChanged();
+            }
+        }
+        
+        // if (_link is { Accounts: not null and { Count: > 0 } })
+        // {
+        //     var accountMetadataTasks = _link!.Accounts.Select(accountId => _client.GetAccountMetadata(accountId)).ToArray();
+        //     var results = await Task.WhenAll(accountMetadataTasks);
+        //     _accounts = results.Where(account => account is not null).Select(a => a).ToList();
+        // }
+    }
+
+    private string GetRowClass(int i)
+    {
+        return GetSelected() == i ? "bg-secondary text-white" : string.Empty;
+    }
+
+    protected override List<Guid>? GetList()
+    {
+        return _link?.Accounts;
+    }
 }
