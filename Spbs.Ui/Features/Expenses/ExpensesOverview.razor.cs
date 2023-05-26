08fd2ab1af3e04f07afcaf6d8e1233f63378945f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-05-26 16:21:15 +0200

    Tweak of date time filtering of expenses, add filtering logic to grid.

--- Spbs.Ui/Features/Expenses/ExpensesOverview.razor.cs
+++ Spbs.Ui/Features/Expenses/ExpensesOverview.razor.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
 using BlazorBootstrap;
 using Microsoft.AspNetCore.Components;
@@ -11,69 +13,52 @@ using Spbs.Ui.Components;
 
 namespace Spbs.Ui.Features.Expenses;
 
-public class ExpenseFilter 
-{
-    public DateTime? FromDate { get; set; }
-    public bool FromDateMonthOnly { get; set; } = false;
-}
-
 [AuthenticationTaskExtension()]
-public partial class ExpensesOverview : SelectableListComponent<Expense>
+public partial class ExpensesOverview : ComponentBase
 {
-    private List<Expense>? _expenses = null;
-    protected override List<Expense>? GetList() => _expenses;
-
+    private DateTime _filterLowerBound;
+    private bool _filterDateHasChanged;
+    private List<Expense> _cachedExpenses = new();
+    
+#pragma warning disable CS8618
     private Grid<Expense> _expensesGrid;
-    
-    [Inject] public IExpenseReaderRepository ExpenseRepository { get; set; }
-
-    private ExpenseFilter _expenseFilter { get; set; } = new ExpenseFilter();
-    
     private EditExpenseComponent _editExpenseComponent;
-    
-    public ExpensesOverview()
-    {
-        
-    }
+    [Inject] public IExpenseReaderRepository ExpenseRepository { get; set; }
+#pragma warning restore CS8618
 
-    protected override async Task OnInitializedAsync()
+    protected override void OnInitialized()
     {
-        await FetchExpenses();
-    }
-
-    private async Task FetchExpenses()
-    {
-        Guid? userId = await UserId();
-        _expenses ??= new();
-        if (_expenseFilter is { FromDate: not null })
-        {
-            _expenses = await ExpenseRepository.GetSingleExpensesByUserAndMonth(userId.Value, _expenseFilter.FromDate.Value);
-        }
-        else
-        {
-            _expenses = await ExpenseRepository.GetSingleExpensesByUser(userId.Value);
-        }
+        _filterLowerBound = DateTime.UtcNow;
+        _filterDateHasChanged = true;
     }
 
     private async Task<GridDataProviderResult<Expense>> FetchExpenses(GridDataProviderRequest<Expense> request)
     {
         var filters = _expensesGrid?.GetFilters();
-        if (filters is null)
+        if (filters is not null)
         {
-            return new() {Data = new List<Expense>()};
-        }
-        
-        foreach (var f in filters)
-        {
-            Console.WriteLine(f.PropertyName);
-            Console.WriteLine(f.Operator);
-            Console.WriteLine(f.Value);
-            Console.WriteLine();
+            foreach (var f in filters)
+            {
+                if (f.PropertyName == "Date")
+                {
+                    DateTime newFilterCandidate = DateTime.Parse(f.Value);
+                    if (newFilterCandidate != _filterLowerBound)
+                    {
+                        _filterLowerBound = newFilterCandidate;
+                        _filterDateHasChanged = true;
+                    }
+                }
+            }            
         }
 
-        await FetchExpenses();
+        if (_filterDateHasChanged)
+        {
+            Guid? userId = await UserId();
+            _cachedExpenses = await ExpenseRepository.GetSingleExpensesByUserAndMonth(userId!.Value, _filterLowerBound);
+            _filterDateHasChanged = false;
+        }
         
-        return new(){Data = _expenses!, TotalCount =_expenses!.Count };
+        return request.ApplyTo(_cachedExpenses);
     }
 
     public string GetExpenseDetailsUrl(Expense e)
@@ -83,31 +68,20 @@ public partial class ExpensesOverview : SelectableListComponent<Expense>
 
     private async void ExpenseItemAdded()
     {
-        await FetchExpenses();
-        StateHasChanged();
+        //await FetchExpenses();
+        //StateHasChanged();
     }
     
     private void ToggleExpenseDialog()
     {
-        Expense? e = null;
-        int? selectedRow = GetSelected();
-        if (selectedRow is not null && selectedRow >= 0 && selectedRow < _expenses?.Count)
-        {
-            e = _expenses?[selectedRow.Value];
-        }
-        
-        _editExpenseComponent?.SetModalContent(e);
-        _editExpenseComponent?.ShowModal();
-    }
-
-    private string GetRowClass(int i)
-    {
-        return GetSelected() == i ? "bg-secondary text-white" : string.Empty;
-    }
-
-    private async void ApplyFilter()
-    {
-        await FetchExpenses();
-        StateHasChanged();
+        // Expense? e = null;
+        // int? selectedRow = GetSelected();
+        // if (selectedRow is not null && selectedRow >= 0 && selectedRow < _expenses?.Count)
+        // {
+        //     e = _expenses?[selectedRow.Value];
+        // }
+        //
+        // _editExpenseComponent?.SetModalContent(e);
+        // _editExpenseComponent?.ShowModal();
     }
 }
