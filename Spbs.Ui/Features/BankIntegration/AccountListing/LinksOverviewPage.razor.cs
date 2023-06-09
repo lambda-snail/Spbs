4ded9ad563c6855ffb41df54c707d04105f3751a
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-09 16:05:56 +0200

    Overhaul of the links overview page

--- Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
+++ Spbs.Ui/Features/BankIntegration/AccountListing/LinksOverviewPage.razor.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
@@ -18,7 +19,7 @@ using Spbs.Ui.Features.BankIntegration.Services;
 namespace Spbs.Ui.Features.BankIntegration.AccountListing;
 
 [AuthenticationTaskExtension]
-public partial class LinksOverviewPage : SelectableListComponent<NordigenLink>
+public partial class LinksOverviewPage : ComponentBase
 {
     [Inject, MaybeNull] private INordigenLinkReaderRepository _linkReader { get; set; }
     [Inject, MaybeNull] private INordigenLinkWriterRepository _linkWriter { get; set; }
@@ -26,22 +27,16 @@ public partial class LinksOverviewPage : SelectableListComponent<NordigenLink>
     [Inject, MaybeNull] private INotificationService _notificationService { get; set; }
     [Inject, MaybeNull] private IMapper _mapper { get; set; }
     [Inject, MaybeNull] private IRedirectLinkService _redirectService { get; set; }
-    
-    protected override List<NordigenLink>? GetList() => _userLinks?.ToList();
-    
-    private ReadOnlyCollection<NordigenLink>? _userLinks;
+
+    private ReadOnlyCollection<NordigenLink>? _userLinks = new(new List<NordigenLink>());
     private ReadOnlyCollection<Institution>? _institutions;
-    
+    private HashSet<NordigenLink> _selectedLinks = new();
+
     protected override async Task OnInitializedAsync()
     {
         await InitInstitutionList();
         await GetLinksForUser();
         StateHasChanged();
-    }
-    
-    private string GetRowClass(int i)
-    {
-        return GetSelected() == i ? "bg-secondary text-white" : string.Empty;
     }
 
     private async Task GetLinksForUser()
@@ -58,38 +53,46 @@ public partial class LinksOverviewPage : SelectableListComponent<NordigenLink>
         // TODO: Remove hard coded country
         var aspsps = await _nordigenCLient!.GetListOfInstitutionsAsync("SE");
         var institutions = _mapper!.Map<List<Institution>>(aspsps);
-        
+
 #if DEBUG
         institutions.Add(new Institution { Name = "Sandbox", Id = "SANDBOXFINANCE_SFIN0000", Bic = string.Empty });
 #endif
-        
+
         _institutions = new(institutions);
     }
 
-    private async Task DeleteSelectedLink()
+    private async Task DeleteSelectedLinks()
     {
-        int? selectedLink = GetSelected();
-        if (_userLinks is null or { Count: 0 } || selectedLink is null)
+        if (_selectedLinks is not { Count: >0 })
         {
             return;
         }
 
-        var link = _userLinks[selectedLink.Value];
-        await _linkWriter!.DeleteLink(link);
-
-        var left = _userLinks.Take(selectedLink.Value).ToList();
-        var right = _userLinks.Skip(selectedLink.Value + 1);
-        
-        left.AddRange(right as NordigenLink[] ?? right.ToArray());
-        _userLinks = new(left);
-
-        if (link is { NordigenId: not null })
+        foreach (var link in _selectedLinks)
         {
-            _nordigenCLient?.DeleteRequisition(link.NordigenId.Value);            
+            await _linkWriter!.DeleteLink(link);
+
+            if (link is { NordigenId: not null })
+            {
+                _nordigenCLient?.DeleteRequisition(link.NordigenId.Value);
+            }
+
+            // TODO: Error handling
+            _notificationService?.ShowToast("Link Deleted", $"Link to {link.InstitutionId} was successfully deleted!",
+                NotificationLevel.Success);
         }
-        
-        // TODO: Error handling
-        _notificationService?.ShowToast("Link Deleted", $"Link to {link.InstitutionId} was successfully deleted!", NotificationLevel.Success);
+
+        // Slightly inefficient, but the user will probably not delete that many at a time so should be ok :)
+        _userLinks = new(_userLinks!.Where(l => !_selectedLinks.Contains(l)).ToList());
+
+        _selectedLinks.Clear();
+
         StateHasChanged();
+    }
+
+    private Task OnSelectedItemsChanged(HashSet<NordigenLink>? selection)
+    {
+        _selectedLinks = selection is not null && selection.Any() ? selection : new();
+        return Task.CompletedTask;
     }
 }
