52b35f8555186da4f4b33cbeb3afd7255e09229a
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-11 15:14:56 +0200

    Add repository for user settings

--- Spbs.Ui/Features/Users/User.cs
+++ Spbs.Ui/Features/Users/User.cs
@@ -1,9 +1,14 @@
 using System;
+using Spbs.Ui.Data.Cosmos;
 
 namespace Spbs.Ui.Features.Users;
 
-public class User
+public class User : ICosmosData
 {
     public Guid Id { get; set; }
-    public Guid AzureAdId { get; set; }
+    public Guid UserId { get; set; }
+
+    public string TestName { get; set; }
+
+    public DateTime ModifiedOn { get; set; }
 }
