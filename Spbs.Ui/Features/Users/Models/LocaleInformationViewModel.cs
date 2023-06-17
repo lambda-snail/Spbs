49b8ac7405fcd2f89935e6b218673f3f8ecd9da1
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-17 15:12:36 +0200

    Add view model and form for locale settings

---
+++ Spbs.Ui/Features/Users/Models/LocaleInformationViewModel.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Spbs.Ui.Features.Users;
+
+public class LocaleInformationViewModel
+{
+    public string TimeZone { get; set; }
+}
---
+++ Spbs.Ui/Features/Users/UserLocaleSettingsComponent.razor.cs
@@ -0,0 +1,27 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Components;
+
+namespace Spbs.Ui.Features.Users;
+
+public partial class UserLocaleSettingsComponent : UserSettingsComponentBase
+{
+    private LocaleInformationViewModel _localeInformationVm { get; set; } = new();
+
+    [Inject] private IMapper _mapper { get; set; }
+
+    protected override void OnInitialized()
+    {
+        _localeInformationVm = _mapper.Map<LocaleInformationViewModel>(UserObject.LocaleInformation);
+    }
+
+    private void HandleValidSubmit()
+    {
+        UserObject.LocaleInformation = _mapper.Map<LocaleInformation>(_localeInformationVm);
+        UserSettingsChangedCallback?.Invoke();
+    }
+
+    private void HandleInvalidSubmit()
+    {
+        
+    }
+}
