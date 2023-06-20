47ce2140b6d7f07e63b098aee1f5e67d14425cbc
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-20 20:45:45 +0200

    Add category property to expenses

---
+++ Spbs.Ui/Features/Expenses/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Spbs.Ui.Data.Cosmos;
+
+namespace Spbs.Ui.Features.Expenses.Models;
+
+/// <summary>
+/// A subset of the full user to fetch only what is needed by the expense feature.
+/// </summary>
+public class ExpenseUser : ICosmosData
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    
+    public List<string> ExpenseCategories { get; set; } = new();
+}
