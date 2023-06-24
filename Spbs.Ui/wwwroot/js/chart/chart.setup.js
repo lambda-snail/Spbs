070dd7f0698262966a17bdfc45c6ba21d7c79f37
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-24 19:12:42 +0200

    Add data manipulating functions to teh chart integration

--- Spbs.Ui/wwwroot/js/chart/chart.setup.js
+++ Spbs.Ui/wwwroot/js/chart/chart.setup.js
@@ -7,3 +7,19 @@
         console.log(e);
     }
 }
+
+function addDataToChart(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function clearChart(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
