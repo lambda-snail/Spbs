7d36638ef58aab608a1ba47c262ed8cd8befe947
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-26 00:51:57 +0200

    Update expense overview toolbar

--- Spbs.Ui/Features/Expenses/ExpensesOverview.razor.cs
+++ Spbs.Ui/Features/Expenses/ExpensesOverview.razor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using Spbs.Generators.UserExtensions;
 
 namespace Spbs.Ui.Features.Expenses;
@@ -11,10 +12,14 @@ public partial class ExpensesOverview : ComponentBase
 {
     private DateTime? _dateFilterYearMonth;
     private List<Expense> _cachedExpenses = new();
+
+    private int _numSelectedExpenses = 0;
     
 #pragma warning disable CS8618
     private EditExpenseComponent _editExpenseComponent;
-    [Inject] public IExpenseReaderRepository ExpenseRepository { get; set; }
+    private MudDataGrid<Expense> _grid;
+    [Inject] private IExpenseReaderRepository _expenseReader { get; set; }
+    [Inject] private ISnackbar _snackBar { get; set; }
 #pragma warning restore CS8618
 
     protected override async Task OnInitializedAsync()
@@ -34,7 +39,7 @@ public partial class ExpensesOverview : ComponentBase
         }
         
         Guid? userId = await UserId();
-        _cachedExpenses = await ExpenseRepository.GetMonthlyExpensesForUser(userId!.Value, date.Value);
+        _cachedExpenses = await _expenseReader.GetMonthlyExpensesForUser(userId!.Value, date.Value);
     }
 
     public string GetExpenseDetailsUrl(Expense e)
@@ -52,5 +57,22 @@ public partial class ExpensesOverview : ComponentBase
     {
         _editExpenseComponent?.SetModalContent(null);
         _editExpenseComponent?.ShowModal();
+    }
+
+    private void DeleteExpenses()
+    {
+        var selectedItems = _grid.SelectedItems;
+        if (selectedItems is { Count: 0 })
+        {
+            // We shouldn't get here, but just in case
+            _snackBar.Add("No expenses selected", Severity.Warning);
+        }
+        
+        _snackBar.Add($"Deleted {selectedItems.Count} {(selectedItems.Count > 1 ? "expenses" : "expense")}", Severity.Success);
+    }
+
+    private void SelectedItemsChanged(HashSet<Expense> selection)
+    {
+        _numSelectedExpenses = selection.Count;
     }
 }
