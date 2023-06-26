9a34967616a843abc158347f264c538a020ce230
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-26 12:38:53 +0200

    Update the expense overview form

--- Spbs.Ui/Features/Expenses/EditExpenseComponent.razor.cs
+++ Spbs.Ui/Features/Expenses/EditExpenseComponent.razor.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using AutoMapper;
+using FluentValidation;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using Spbs.Generators.UserExtensions;
 using Spbs.Ui.Features.Users;
 using Spbs.Ui.Features.Users.Repositories;
+using Shared.Utilities;
+using Severity = MudBlazor.Severity;
 
 namespace Spbs.Ui.Features.Expenses;
 
@@ -20,14 +23,18 @@ public partial class EditExpenseComponent : ComponentBase
     private EditExpenseViewModel _editExpenseViewModel = new() { Date = DateTime.Now };
 
     private List<string> _expenseCategories = new();
-    
+
 #pragma warning disable CS8618
     [Inject] private IMapper _mapper { get; set; }
     [Inject] private IExpenseWriterRepository _expenseWriterRepository { get; set; } 
     [Inject] private IUserRepository _userRepository { get; set; }
-    
+    [Inject] private IValidator<EditExpenseViewModel> _expenseValidator { get; set; }
+    [Inject] private ISnackbar _snackbar { get; set; }
+
     [Parameter, Required] public Func<Guid?> GetUserId { get; set; }
     [Parameter] public Action OnUpdateCallback { get; set; }
+    
+    private MudForm _form;
 #pragma warning restore CS8618
     
     public void ShowModal()
@@ -68,6 +75,20 @@ public partial class EditExpenseComponent : ComponentBase
 
     private async Task HandleValidSubmit()
     {
+        await _form.Validate();
+        if (_form.IsValid)
+        {
+            _snackbar.Add("Changes saved!");
+        }
+        else
+        {
+            _snackbar.Add("An error occured while saving", Severity.Error);
+        }
+
+        CloseDialog();
+        
+        return;
+        
         Expense expense;
         if (_expense is null)
         {
