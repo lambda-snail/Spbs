dce7f1bf23f80b5219ec5a5088f19880c392d941
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-26 19:37:33 +0200

    Add edit expense item functionality through button and grid click

--- Spbs.Ui/Features/Expenses/ExpenseDetails.razor.cs
+++ Spbs.Ui/Features/Expenses/ExpenseDetails.razor.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.AspNetCore.Components;
@@ -13,6 +15,8 @@ public partial class ExpenseDetails : ComponentBase
 {
     [Parameter] public string ExpenseId { get; set; }
     private Expense? _expense = null;
+    
+    private int _numSelectedExpenseItems = 0;
     
 #pragma warning disable CS8618
     [Inject] private IExpenseReaderRepository _expenseReaderRepository { get; set; }
@@ -76,17 +80,33 @@ public partial class ExpenseDetails : ComponentBase
         _editExpenseComponent?.ShowModal();
     }
 
-    private void AddExpenseItem()
+    private async Task AddExpenseItem()
     {
         var newItem = new ExpenseItem { Id = Guid.NewGuid() };
         
         _expense!.Items.Add(newItem);
-        _grid.ReloadServerData();
-        _grid.SetEditingItemAsync(newItem);
+        await _grid.ReloadServerData();
+        await  _grid.SetEditingItemAsync(newItem);
     }
 
     private Task OnExpenseItemChanged()
     {
         return SaveExpense();
+    }
+    
+    private async Task EditSelectedItem()
+    {
+        var selectedItems = _grid.SelectedItems;
+        if (selectedItems is { Count: not 1 })
+        {
+            _snackbar.Add("Too many expenses selected", Severity.Warning);
+        }
+
+        await  _grid.SetEditingItemAsync(selectedItems.First());
+    }
+    
+    private void OnSelectedItemsChanged(HashSet<ExpenseItem> selection)
+    {
+        _numSelectedExpenseItems = selection.Count;
     }
 }
