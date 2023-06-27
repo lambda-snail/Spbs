e6543338d77c596c0179912060655515503254e9
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-27 20:16:15 +0200

    Mudify edit dialog for recurring expenses

--- Spbs.Ui/Features/RecurringExpenses/ViewModels/Validation/EditRecurringExpenseViewModelFluentValidation.cs
+++ Spbs.Ui/Features/RecurringExpenses/ViewModels/Validation/EditRecurringExpenseViewModelFluentValidation.cs
@@ -7,6 +7,8 @@ public class EditRecurringExpenseViewModelFluentValidation : AbstractValidator<E
     public EditRecurringExpenseViewModelFluentValidation()
     {
         RuleFor(e => e.Name).NotEmpty().WithMessage("Please provide a name for the expense");
+        RuleFor(e => e.Description).MaximumLength(4000)
+            .WithMessage("Description too long. The description is capped at 4,000 characters!");
         RuleFor(e => e.BillingDay).InclusiveBetween(1, 31).WithMessage("The billing day must be a day of the month");
         RuleFor(e => e.BillingPrincipal).NotEmpty().WithMessage("Please provide the name of the recipient");
         RuleFor(e => e.Total).GreaterThan(0).WithMessage("The cost must not be greater than 0");
