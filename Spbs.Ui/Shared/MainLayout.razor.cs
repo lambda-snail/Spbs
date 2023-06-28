177459da7a486eec3706e9567285ed8206e29863
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-06-28 16:16:49 +0200

    Remove some more bootstrap component references

--- Spbs.Ui/Shared/MainLayout.razor.cs
+++ Spbs.Ui/Shared/MainLayout.razor.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using BlazorBootstrap;
 using Microsoft.AspNetCore.Components;
 using Spbs.Generators.UserExtensions;
 
@@ -10,12 +8,6 @@ namespace Spbs.Ui.Shared;
 [AuthenticationTaskExtension]
 public partial class MainLayout : LayoutComponentBase
 {
-    IEnumerable<NavItem>? _navItems;
-
-#pragma warning disable CS8618
-    Sidebar _sidebar;
-#pragma warning restore CS8618
-    
     protected override async Task OnInitializedAsync()
     {
         Guid? userId = await UserId();
