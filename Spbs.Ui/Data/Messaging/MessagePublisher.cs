566254ab190a30b2a1361dc565b1cc512497d2fa
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-07-02 21:11:41 +0200

    Add abstract base class for message publishing

---
+++ Spbs.Ui/Data/Messaging/MessagePublisher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
+namespace Spbs.Ui.Data.Messaging;
+
+public abstract class MessagePublisher<T> : IAsyncDisposable
+{
+    private readonly ServiceBusSender _serviceBusSender;
+
+    /// <summary>
+    /// ctor for mocking
+    /// </summary>
+    protected MessagePublisher() { }
+
+    public MessagePublisher(ServiceBusClient client, string queueName)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(queueName);
+        _serviceBusSender = client.CreateSender(queueName);
+    }
+    
+    public Task PublishMessage(T payload)
+    {
+        var message = CreateMessage(payload);
+        return _serviceBusSender.SendMessageAsync(message);
+    }
+
+    public Task ShceduleMessage(T payload, DateTime deliveryTime)
+    {
+        var message = CreateMessage(payload);
+        return _serviceBusSender.ScheduleMessageAsync(message, deliveryTime.ToUniversalTime());
+    }
+
+    private ServiceBusMessage CreateMessage(T payload) => new ServiceBusMessage( Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(payload)) );
+    
+    public ValueTask DisposeAsync()
+    {
+        return _serviceBusSender.DisposeAsync();
+    }
+}
