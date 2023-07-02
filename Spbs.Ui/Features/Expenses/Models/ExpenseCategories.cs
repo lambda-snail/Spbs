cbca0577c8d0c53b2e045e56933deb3ecc04d7c0
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-07-02 21:23:52 +0200

    Fix typo in some expense categories

--- Spbs.Ui/Features/Expenses/Models/ExpenseCategories.cs
+++ Spbs.Ui/Features/Expenses/Models/ExpenseCategories.cs
@@ -15,13 +15,13 @@ public class ExpenseCategories
     public static readonly Category Housing = new() { Name = "Housing", MudIcon = Icons.Material.Filled.House };
     public static readonly Category Transportation = new() { Name = "Transportation", MudIcon = Icons.Material.Filled.House };
     public static readonly Category Food = new() { Name = "Food", MudIcon = Icons.Material.Filled.LocalDining };
-    public static readonly Category DiningOut = new() { Name = "DiningOut", MudIcon = Icons.Material.Filled.Fastfood };
+    public static readonly Category DiningOut = new() { Name = "Dining Out", MudIcon = Icons.Material.Filled.Fastfood };
     public static readonly Category Utilities = new() { Name = "Utilities", MudIcon = Icons.Material.Filled.ElectricMeter };
     public static readonly Category Insurance = new() { Name = "Insurance", MudIcon = Icons.Material.Filled.HealthAndSafety };
     public static readonly Category Healthcare = new() { Name = "Healthcare", MudIcon = Icons.Material.Filled.LocalHospital };
     public static readonly Category SavingsInvestment = new() { Name = "Savings & Investment", MudIcon = Icons.Material.Filled.Savings };
-    public static readonly Category DebtPayment = new() { Name = "DebtPayment", MudIcon = Icons.Material.Filled.MonetizationOn };
-    public static readonly Category PersonalSpending = new() { Name = "PersonalSpending", MudIcon = Icons.Material.Filled.SelfImprovement };
+    public static readonly Category DebtPayment = new() { Name = "Debt Payment", MudIcon = Icons.Material.Filled.MonetizationOn };
+    public static readonly Category PersonalSpending = new() { Name = "Personal Spending", MudIcon = Icons.Material.Filled.SelfImprovement };
     public static readonly Category Entertainment = new() { Name = "Entertainment", MudIcon = Icons.Material.Filled.TheaterComedy };
     public static readonly Category Misc = new() { Name = "Misc", MudIcon = Icons.Material.Filled.Bento };
     public static readonly Category ChildCare = new() { Name = "ChildCare", MudIcon = Icons.Material.Filled.ChildFriendly };
