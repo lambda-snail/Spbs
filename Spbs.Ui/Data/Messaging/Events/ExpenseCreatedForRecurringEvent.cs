2fcfa18cdef04f499a35c9618c310010a251264b
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2023-07-03 12:17:23 +0200

    Create messages for recurring expense creation

---
+++ Spbs.Ui/Data/Messaging/Events/ExpenseCreatedForRecurringEvent.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Spbs.Ui.Data.Messaging.Events;
+
+public class ExpenseCreatedForRecurringEvent
+{
+    public Guid ExpenseId { get; set; }
+    public Guid RecurringExpenseId { get; set; }
+    public DateTime Date { get; set; }
+    public double Total { get; set; }
+}
---
+++ Spbs.Ui/Data/Messaging/Messages/CreateExpenseCommand.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Spbs.Ui.Data.Messaging.Messages;
+
+public class CreateExpenseCommand
+{
+    /// <summary>
+    /// Which queue to post the resulting ExpenseCreatedEvent
+    /// </summary>
+    public string ReplyQueueName { get; set; }
+    public CreateExpenseCommandPayload Expense { get; set; }
+}
+
+public class CreateExpenseCommandPayload
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    
+    public bool Recurring { get; set; } = false;
+    public Guid RecurringExpenseId { get; set; }
+    
+    public DateTime Date { get; set; }
+    public string? Venue { get; set; }
+    public string? Category { get; set; }
+
+    public double Total { get; set; }
+    public string Currency { get; set; }
+}
